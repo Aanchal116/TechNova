e603e65f094e9fc1dd6bc10d82d635a463e4cadf
Brianna <brianna.briibrii@gmail.com>
2021-08-28 12:57:25 -0700

    starting of login page
    login page starter so we can begin with google auth

---
+++ procrastinot/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Login from './components/login'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>ProcrastiNot Scheduler</h1>
+      </header>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
---
+++ procrastinot/src/components/login.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+export default class Login extends Component {
+
+
+    render() {
+        return (
+            <div className="card" >
+                <h4>Login in with Google</h4>
+                <button className="login">Login</button>
+            </div>
+        )
+    }
+}
